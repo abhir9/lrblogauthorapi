02ad34d26e20fab5e84cc7654170835105e17fec
abhimanyu singh rathore <abhimanyu.rathore@loginradius.com>
2021-04-25 12:59:08 +0530

    added cors

--- app.js
+++ app.js
@@ -3,9 +3,8 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-
+var cors = require("cors");
 var indexRouter = require('./routes/index');
-
 var app = express();
 
 // view engine setup
@@ -17,6 +16,8 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(cors())
 
 app.use('/', indexRouter);
 
